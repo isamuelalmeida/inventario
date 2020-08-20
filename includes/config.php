ab034e049f771bbf4d9f096ba5a87193140bfd29
Alexi Lalas Ribeiro Pereira <alexi@stc.ma.gov.br>
2020-08-20 09:36:49 -0300

    Ajustes no script do sql e adição do script datatables

--- includes/config.php
+++ includes/config.php
@@ -13,7 +13,7 @@
 */
   define( 'DB_HOST', 'localhost' );          // Set database host
   define( 'DB_USER', 'root' );             // Set database user
-  define( 'DB_PASS', '1234' );             // Set database password
+  define( 'DB_PASS', 'root' );             // Set database password
   define( 'DB_NAME', 'inventario' );        // Set database name
 
 ?>
--- layouts/footer.php
+++ layouts/footer.php
@@ -1,10 +1,42 @@
      </div>
     </div>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
   <script src="//cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.3.0/js/bootstrap-datepicker.min.js"></script>
+  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
+  <script src="//cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
   <script type="text/javascript" src="assets/js/functions.js"></script>
   </body>
 </html>
+<script>
+  $(document).ready( function () {
+    if($('.table').length)
+    $('.table').DataTable({
+      "bJQueryUI": true,
+      "sPaginationType": "full_numbers",
+      "pageLength": 10,
+      "lengthMenu": [ [12, 24, 36, -1], [12, 24, 36, "All"] ],
+      "oLanguage": {
+          "sLengthMenu": "",
+          //"sLengthMenu": "Mostrar _MENU_ registros por página",
+          "sZeroRecords": "Nenhum registro encontrado",
+          "sInfo": "Mostrando _START_ / _END_ de _TOTAL_ registro(s)",
+          "sInfoEmpty": "Mostrando 0 / 0 de 0 registros",
+          "sInfoFiltered": "(filtrado de _MAX_ registros)",
+          "sSearch": "Pesquisar",
+          "oPaginate": {
+              "sFirst": "Início",
+              "sPrevious": "Anterior",
+              "sNext": "Próximo",
+              "sLast": "Último"
+          }
+      },
+      "aaSorting": [[0, 'asc']],
+      "aoColumnDefs": [
+          {"orderable": false}
 
+      ]
+    });
+  } );
+  </script>
 <?php if(isset($db)) { $db->db_disconnect(); } ?>
