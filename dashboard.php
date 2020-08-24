0502ee7306beb6a5cc0e08b6dc3407f63caa3db2
Alexi Lalas Ribeiro Pereira <alexi@stc.ma.gov.br>
2020-08-24 10:01:19 -0300

    Charts inserted on dashboard

--- dashboard.php
+++ dashboard.php
@@ -14,6 +14,8 @@
 
 ?>
 <?php include_once('layouts/header.php'); ?>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.bundle.min.js" integrity="sha512-vBmx0N/uQOXznm/Nbkp7h0P1RfLSj0HQrFSzV8m7rOGyj30fYAOKHYvCNez+yM8IrfnW0TCodDEjRqf6fodf/Q==" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.css" integrity="sha512-/zs32ZEJh+/EO2N1b0PEdoA10JkdC3zJ8L5FTiQu82LR9S/rOQNfQN7U59U9BC12swNeRAz3HSzIL2vpp4fv3w==" crossorigin="anonymous" />
 
 <div class="row">
    <div class="col-md-6">
@@ -66,6 +68,20 @@
        </div>
     </div>
 </div>
+<hr>
+<!-- Charts -->
+<div class="row" style="background-color:white">
+  <div>
+    <div class="col-md-7">
+      <canvas id="pieChart" width="500" height="200"></canvas>
+    </div>
+    <div class="col-md-5">
+      <canvas id="barChart" width="300" height="200"></canvas>
+    </div>
+  </div>
+</div>
+<!-- /Charts -->
+<hr>
   <div class="row">
    <div class="col-md-12">
       <div class="panel">
@@ -142,6 +158,76 @@
 
   </div>
 
+<script>
+  var ctx = document.getElementById('pieChart');
+  var myChart = new Chart(ctx, {
+      type: 'pie',
+      data: {
+          labels: ['PC', 'Notebook', 'AIO', 'AP', 'Switch', 'Estabilizador'],
+          datasets: [{
+              label: 'Equipamentos',
+              data: [12, 19, 3, 5, 2, 3],
+              backgroundColor: [
+                  'rgba(255, 99, 132, 0.2)',
+                  'rgba(54, 162, 235, 0.2)',
+                  'rgba(255, 206, 86, 0.2)',
+                  'rgba(75, 192, 192, 0.2)',
+                  'rgba(153, 102, 255, 0.2)',
+                  'rgba(255, 159, 64, 0.2)'
+              ],
+              borderColor: [
+                  'rgba(255, 99, 132, 1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(75, 192, 192, 1)',
+                  'rgba(153, 102, 255, 1)',
+                  'rgba(255, 159, 64, 1)'
+              ],
+              borderWidth: 1,
+          }]
+      },
+  });
 
+
+  // bar chart
+
+  var ctx = document.getElementById('barChart');
+  var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Suafin', 'Seatran', 'Asplae', 'ASCOM', 'RH', 'Financeiro'],
+        datasets: [{
+            label: 'Empréstimos por Setor',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+</script>
 
 <?php include_once('layouts/footer.php'); ?>
